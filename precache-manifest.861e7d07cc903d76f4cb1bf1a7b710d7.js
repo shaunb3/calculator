a4104be158176acb5e6096a809b87e06d8db2df1
Shaun <shaunbw4@gmail.com>
2021-03-10 01:09:20 +0200

    Updates

---
+++ precache-manifest.861e7d07cc903d76f4cb1bf1a7b710d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e354ba75048e086cdce406501f20bbc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "73e84708fc990b65aba1",
+    "url": "/calculator/static/css/main.dbb398fb.chunk.css"
+  },
+  {
+    "revision": "de6d1f5ee44d485a3114",
+    "url": "/calculator/static/js/2.9d1bed5c.chunk.js"
+  },
+  {
+    "revision": "73e84708fc990b65aba1",
+    "url": "/calculator/static/js/main.7e09c54f.chunk.js"
+  },
+  {
+    "revision": "8b2e9f5720cce1ab0d2a",
+    "url": "/calculator/static/js/runtime-main.26f27520.js"
+  }
+]);
